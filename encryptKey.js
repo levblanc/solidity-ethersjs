b211182b5d212398c91563f1aeed8c68df0b71cf
levblanc <levblanc@gmail.com>
2022-09-05 17:32:38 +0800

    Better private key management with encryption

---
+++ encryptKey.js
@@ -0,0 +1,23 @@
+const ethers = require('ethers');
+const fs = require('fs-extra');
+require('dotenv').config();
+
+async function encryptKey() {
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
+  const encryptedJsonKey = await wallet.encrypt(
+    process.env.PRIVATE_KEY_PASSWORD,
+    process.env.PRIVATE_KEY
+  );
+
+  try {
+    fs.writeFileSync('./encryptKey.json', encryptedJsonKey);
+    console.log('>>>>>> Encryption success!');
+  } catch (error) {
+    console.log('>>>>>> Encryption falied!');
+    console.error(error);
+  }
+}
+
+encryptKey()
+  .then(() => process.exit(0))
+  .catch(() => process.exit(1));
